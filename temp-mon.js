120bcf71f611e3e6c5b86c5647b7644fd105b0a2
Michael Ta <technical.ta@gmail.com>
2016-05-05 10:53:44 -0400

    add cloud connect

--- temp-mon.js
+++ temp-mon.js
@@ -60,6 +60,8 @@ if (dht_sensor.initialize()) {
     console.warn('Failed to initialize sensor');
 }
 
+piREST.connect();
+
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
